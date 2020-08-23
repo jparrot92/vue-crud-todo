422500b75204794a90b617d7bc3ee6076ade32b4
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-08-23 20:10:17 +0200

    Iniciar componente TodoItem y escribir la lógica necesaria

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,36 @@
+<template>
+
+</template>
+
+<script>
+import { mapMutations, mapActions } from 'vuex';
+
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    ...mapActions({
+      _updateTodoStatus: 'todos/updateTodoStatus',
+      _removeTodo: 'todos/removeTodo',
+    }),
+    ...mapMutations('todos', ['setTodo']),
+    goToUpdateTodo() {
+      this.setTodo(this.todo);
+      this.$router.push({
+        name: 'todos-update',
+        params: { id: this.todo.id },
+      });
+    },
+    updateTodoStatus() {
+      this._updateTodoStatus(this.todo);
+    },
+    removeTodo() {
+      this._removeTodo(this.todo.id);
+    },
+  },
+};
+</script>
