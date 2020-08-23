b01684dce5f4629e56059b93d42c0e0db4743c89
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-08-23 20:17:13 +0200

    Finalizar componente TodoItem, escribir la template y utilizar en TodoList

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,5 +1,30 @@
 <template>
-
+    <b-row class="mb-2">
+        <b-col cols="2">{{ todo.text }}</b-col>
+        <b-col cols="1">{{ todo.done }}</b-col>
+        <b-col>
+            <b-button
+                variant="primary"
+                @click="goToUpdateTodo"
+            >
+                Editar
+            </b-button>
+            <b-button
+                variant="warning"
+                class="ml-2"
+                @click="updateTodoStatus"
+            >
+                Estado
+            </b-button>
+            <b-button
+                variant="danger"
+                class="ml-2"
+                @click="removeTodo"
+            >
+                Eliminar
+            </b-button>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,21 @@
 <template>
     <div v-if="todos.length">
-
+        <todo-item
+            v-for="todo in todos"
+            :key="todo.id"
+            :todo="todo"
+        ></todo-item>
     </div>
     <b-alert show variant="info" v-else>No hay todos</b-alert>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue';
 
 export default {
+  components: {
+    TodoItem,
+  },
   props: {
     todos: {
       type: Array,
