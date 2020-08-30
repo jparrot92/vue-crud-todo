4da2370caa0fa3b6829e8319853dad9f9204280f
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-08-30 23:21:08 +0200

    Desarrollar lógica del Componente TodoForm

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import { validationMixin } from 'vuelidate';
+import { required, minLength } from 'vuelidate/lib/validators';
+
+export default {
+  mixins: [validationMixin],
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+    todoSubmit: {
+      type: String,
+      default: 'Crear todo',
+    },
+  },
+  validations: {
+    todo: {
+      text: {
+        required, minLength: minLength(4),
+      },
+    },
+  },
+};
+</script>
